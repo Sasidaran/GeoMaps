cec1908bfd8764e491e0122fffdadb92b427c82e
Sasidaran S <sasidaran.s@tcs.com>
2016-10-17 08:25:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoMaps::Application.config.secret_token = '4b1d1f479ddd98aaf6326db04f614a2f9e056d7c706a7e67ef4986b99ff286e83845b1dc57a9aae87dba90eadefccdd31ec584f878899c0501c8d2053d7ff73f'
